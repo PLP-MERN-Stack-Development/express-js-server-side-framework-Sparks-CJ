1b86d155bda71f914951a584f9a90086b7729a32
Celestine Jepkemboi <celestinejepkemboi4@gmail.com>
2025-10-22 09:52:57 +0300

    Create errorHandler.js

---
+++ middleware/errorHandler.js
@@ -0,0 +1,8 @@
+// middleware/errorHandler.js
+module.exports = (err, req, res, next) => {
+  console.error('Error:', err.message);
+  res.status(500).json({
+    message: 'Internal Server Error',
+    error: err.message,
+  });
+};
